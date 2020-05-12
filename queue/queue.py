4d4fe4c495a2e63c130da9259d9a2429f88b0185
ivaben <ivaben@users.noreply.github.com>
2020-05-11 20:26:42 -0400

    finished assignment

--- queue/queue.py
+++ queue/queue.py
@@ -13,13 +13,19 @@ return elements in First In First Out order.
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = []
     
     def __len__(self):
-        pass
+        return self.size
 
     def enqueue(self, value):
-        pass
-
+        self.size += 1
+        self.storage.insert(0,value) 
+        
     def dequeue(self):
-        pass
+        if self.size == 0:
+            return
+        else:
+            self.size -= 1
+            return self.storage.pop()
+        
