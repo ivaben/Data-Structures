b1e79194efd2bbc5281049d535b97bf95690acad
ivaben <ivaben@users.noreply.github.com>
2020-05-12 20:51:18 -0400

    finished assignments

--- queue/queue.py
+++ queue/queue.py
@@ -1,6 +1,6 @@
 """
 A queue is a data structure whose primary purpose is to store and
-return elements in First In First Out order. 
+return elements in First In First Out order. (FIFO) 
 
 1. Implement the Queue class using an array as the underlying storage structure.
    Make sure the Queue tests pass.
